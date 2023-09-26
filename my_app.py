aae15ceee072e30e93ea1160a135c6954323485d
Mateusz <mobirek@onet.eu>
2023-09-26 20:29:34 +0200

    next_step

--- my_app.py
+++ my_app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
 
-	return "<h1 style='color:red' >MATEUSZ OBIREK</h1>"
+	return "<h1 style='color:red' >MATEUSZ K</h1>"
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0')
